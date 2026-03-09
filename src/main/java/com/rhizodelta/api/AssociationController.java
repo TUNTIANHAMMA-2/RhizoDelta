626d744ca4f0a2b62822b122ee35b2df33523f39
TUNTIANHAMMA <bliu95922@gmail.com>
2026-03-09 17:41:06 +0800

    [SAL-03-02] Implement DELETE /api/associations/{associationId} endpoint

--- src/main/java/com/rhizodelta/api/AssociationController.java
+++ src/main/java/com/rhizodelta/api/AssociationController.java
@@ -5,10 +5,14 @@ import com.rhizodelta.service.AssociationService;
 import com.rhizodelta.service.CreateAssociationCommand;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/associations")
@@ -24,5 +28,15 @@ public class AssociationController {
         AssociationService.CreateAssociationOutcome outcome = associationService.createAssociation(command);
         HttpStatus status = outcome.created() ? HttpStatus.CREATED : HttpStatus.OK;
         return ResponseEntity.status(status).body(ApiResponse.ok(outcome.association()));
+    }
+
+    @DeleteMapping("/{associationId}")
+    public ResponseEntity<ApiResponse<DeleteAssociationResponse>> delete(@PathVariable("associationId") UUID associationId) {
+        AssociationService.DeleteAssociationOutcome outcome = associationService.deleteAssociation(associationId);
+        DeleteAssociationResponse response = new DeleteAssociationResponse(outcome.association_id(), outcome.deleted());
+        return ResponseEntity.ok(ApiResponse.ok(response));
+    }
+
+    public record DeleteAssociationResponse(UUID association_id, boolean deleted) {
     }
 }
